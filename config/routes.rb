80615217fdf77ee1daf01daaf60c1cfa8ffa4a36
EC2 Default User <ec2-user@ip-172-31-26-131.ap-northeast-1.compute.internal>
2022-07-09 18:43:14 +0000

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists'=>'lists#create'
   get 'lists'=>'lists#index'
-  get '/top' => 'homes#top'
-  get 'lists/edit'
+  get '/top' => 'home#top'
   get 'lists/:id'=>'lists#show',as:'list'
+  get 'lists/:id/edit'=>'lists#edit',as:'edit_list'
+  patch 'lists/:id'=>'lists#update',as:'update_list'
 end
